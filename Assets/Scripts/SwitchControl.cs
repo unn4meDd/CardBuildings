d2e6ff6b34b1b67ac5e3c957caeab54c1be40733
4lexxoranges <or4nges.official@gmail.com>
2021-01-27 21:14:34 +0300

    add transparent material

---
+++ Assets/Scripts/SwitchControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchControl : MonoBehaviour
+{
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        menuPanel.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
