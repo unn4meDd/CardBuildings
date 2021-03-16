5cf819177f8f0bb941a41eff8d72a062297788ce
4lexxoranges <or4nges.official@gmail.com>
2021-03-16 22:02:08 +0300

    .

--- Assets/Scripts/ButtonsControl.cs
+++ Assets/Scripts/ButtonsControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ButtonsControl : MonoBehaviour
 {
+    public MoveCard moveCard;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,27 @@ public class ButtonsControl : MonoBehaviour
     void Update()
     {
         
+    }
+    public void MyBtnHandler()
+    {
+        print(moveCard.needHorizontalMove + " " + moveCard.needRotatingMove + " " + moveCard.needVerticalMove);
+        if(moveCard.needHorizontalMove == true)
+        {
+            moveCard.Change_needHorMove(true, "horMove");
+            // moveCard.StopHorizontalMove();
+        }
+        print(moveCard.needHorizontalMove + " " + moveCard.needRotatingMove + " " + moveCard.needVerticalMove);
+        if(moveCard.needRotatingMove == true)
+        {
+            moveCard.Change_needRotMove(true, "rotMove");
+            // moveCard.StopRotationMove();
+        }
+        print(moveCard.needHorizontalMove + " " + moveCard.needRotatingMove + " " + moveCard.needVerticalMove);
+        if(moveCard.needVerticalMove == true)
+        {
+            moveCard.Change_needVertMove(true, "vertMove");
+            // moveCard.StopVerticalMove();
+        }
+        print(moveCard.needHorizontalMove + " " + moveCard.needRotatingMove + " " + moveCard.needVerticalMove);
     }
 }
--- Assets/Scripts/MoveCard.cs
+++ Assets/Scripts/MoveCard.cs
@@ -17,32 +17,18 @@ public class MoveCard : MonoBehaviour
     Vector3 offset;
     Vector3 offset2;
     private CardMechanics cardMechanics;
-    public GameObject buttonStopHorizontalMove;
-    public GameObject buttonStopRotationMove;
-    public GameObject buttonStopVerticalMove;
-    private bool needHorizontalMove = false;
-    private bool needVerticalMove = false;
-    private bool needRotatingMove = false;
+    public GameObject buttonStopMove;
+    internal bool needHorizontalMove = true;
+    internal bool needVerticalMove = false;
+    internal bool needRotatingMove = false;
     // Start is called before the first frame update
     void Start()
     {
-        // buttonStopHorizontalMove = GameObject.Find("BSHM").GetComponent<GameObject>();
-        // buttonStopRotationMove = GameObject.Find("BSRM").GetComponent<GameObject>();
-        // buttonStopVerticalMove = GameObject.Find("BSVM").GetComponent<GameObject>();
         startPosition = transform.position;
-        needHorizontalMove = true;
-        UpdateActiveBtn();
-    }
-    private void UpdateActiveBtn()
-    {
-        buttonStopHorizontalMove.SetActive(needHorizontalMove);
-        buttonStopRotationMove.SetActive(needRotatingMove);
-        buttonStopVerticalMove.SetActive(needVerticalMove);
     }
     // Update is called once per frame
-    void FixedUpdate()
+    public void Update()
     {
-        print(needHorizontalMove + " " + needRotatingMove + " " + needVerticalMove);
         if(needHorizontalMove == true)
         {
             MoveHorizontal();
@@ -72,27 +58,48 @@ public class MoveCard : MonoBehaviour
         offset2 = movePosition2 * moveProgress;
         transform.position = startPosition + offset + offset2;
     }
+    public void Change_needHorMove(bool newValue, string why)
+    {
+        print("Change_needHorMove");
+        needHorizontalMove = newValue;
+        print("needHorMove " + newValue);
+        print("why " + why);
+    }
+    public void Change_needRotMove(bool newValue, string why)
+    {
+        print("Change_needRotMove");
+        needRotatingMove = newValue;
+        print("needRotMove " + newValue);
+        print("why" + why);
+    }
+    public void Change_needVertMove(bool newValue, string why)
+    {
+        print("Change_needVertMove");
+        needVerticalMove = newValue;
+        print("needVertMove " + newValue);
+        print("why" + why);
+    }
     public void StopHorizontalMove()
     {
+        print("StopHorizontalMove");
         needHorizontalMove = false;
         needRotatingMove = true;
         needVerticalMove = false;
-        UpdateActiveBtn();
     }
     public void StopRotationMove()
     {
+        print("StopRotationMove");
         needRotatingMove = false;
         needVerticalMove = false;
         needVerticalMove = true;
-        UpdateActiveBtn();
     }
     public void StopVerticalMove()
     {
+        print("StopVerticalMove");
         needRotatingMove = false;
         needVerticalMove = false;
-        Destroy(GetComponent<MoveCard>());
-        Instantiate(movingCardExample);
+        var newCard = Instantiate(movingCardExample);
+        Destroy(newCard.GetComponent<MoveCard>());
         needHorizontalMove = true;
-        UpdateActiveBtn();
     }
 }
